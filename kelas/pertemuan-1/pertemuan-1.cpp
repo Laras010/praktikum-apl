7343b042f2571b736826cb90cc27f1053c7499ee
ZihniLarasati <treasuremaker458@gmail.com>
2026-03-02 02:31:07 +0800

    PRAKTIKUM

---
+++ kelas/pertemuan-1/pertemuan-1.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    string nama;
+    string namadua;
+    cout << "masukan nama kamu";
+    cin >> nama;
+    cout << "masukan nama kamu 2";
+    getline(cin, namadua);
+    cout << "halo, " << nama << namadua;
+
+}
+
+
+
+// int main(){
+//     int age = 19;
+//     if (age > 10); {
+//         cout << "dewasa";
+//     } else if (age > 10) {
+//         cout << "remaja";
+//     } else {
+//         cout << "anak anak";
+//     }
+//         return 0;
+// }
+
+// #include <iostream>
+// using namespace std;
+
+// int main(){
+//     cout << "Halo world";
+//     return 0;
+// }
